15cf18bc7ad9910f830d5be3fb278b54268f40b7
MichaelSchwabe <MichaelSchwabe>
2018-11-08 09:25:10 +0100

    PDFATCommands

--- Protokoll/AT-CommandClient.py
+++ Protokoll/AT-CommandClient.py
@@ -3,17 +3,48 @@ import io
 import time
 from _thread import start_new_thread
 
-ser = serial.Serial (
-    port = "/dev/ttyS0")#Open named port)
+
+
+
+
+
+ser = serial.Serial (port = "/dev/ttyS0")#Open named port)
 ser.timeout = 0.1
-ser.baudrate = 115200                     #Set baud rate to 9600
+ser.baudrate = 115200     
+
 
 
 if(not ser.isOpen()):
     ser.open()
 
-print("Initializing the device ..")
 sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
+print("Initializing the device ..")
+def initalConfig():
+    rstAT = "AT+RST"
+    setAddrAT= "AT+ADDR=2020"
+    getAddrAT= "AT+ADDR?"
+    cfgAT = "AT+CFG=433000000,20,6,12,1,1,0,0,0,0,3000,8,4"
+    rxAT = "AT+RX"
+    saveAT = "AT+SAVE"
+    #WriteConfig
+    #sio.write(rstAT + '\r\n')
+    sio.write(cfgAT + '\r\n')
+    sio.flush()
+    sleep(2)
+    sio.write(setAddrAT + '\r\n')
+    sio.flush()
+    sleep(2)
+    sio.write(getAddrAT + '\r\n')
+    sio.flush()
+    sleep(2)
+    sio.write(saveAT + '\r\n')
+    sio.flush()
+    sleep(2)
+    sio.write(rxAT + '\r\n')
+    sio.flush()
+    sleep(2)
+    print("ich wurde ausgeführt")
+
 
 def readSerialLine():
     while 1:
@@ -21,16 +52,13 @@ def readSerialLine():
         if read != "":
             print(read)
 
-def initalConfig():
-    cfgAT = "AT+CFG=433000000,20,6,12,1,1,0,0,0,0,3000,8,4"
-    sio.write(cfgAT + '\r\n')
-    rxAT = "AT+RX"
-    sio.write(rxAT + '\r\n')
-
-#initalConfig()
 start_new_thread(readSerialLine,())
-
-while 1:          
+initalConfig()
+while 1:
+    #i=1
+    #if i == 1:
+        #initalConfig()
+        #i=i+1
     input_val = input("> ")
     if input_val == 'exit':
         ser.close()
@@ -39,5 +67,6 @@ while 1:
         sio.write(input_val + '\r\n')
         sio.flush()
         #print(">>"+sio.readline())
-  
 ser.close()
+
+
