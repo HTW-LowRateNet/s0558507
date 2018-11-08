2541a8dce44205e9cd29701fc0293dd40bc380f6
MichaelSchwabe <MichaelSchwabe>
2018-11-08 09:44:44 +0100

    InitialConfig wird erfolgreich ausgeführt

--- Protokoll/AT-CommandClient.py
+++ Protokoll/AT-CommandClient.py
@@ -3,22 +3,21 @@ import io
 import time
 from _thread import start_new_thread
 
-
-
-
-
-
 ser = serial.Serial (port = "/dev/ttyS0")#Open named port)
 ser.timeout = 0.1
 ser.baudrate = 115200     
-
-
 
 if(not ser.isOpen()):
     ser.open()
 
 sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
 print("Initializing the device ..")
+
+def sendCommand(command):
+    sio.write(command + '\r\n')
+    sio.flush()
+    sio.readline()
+
 def initalConfig():
     rstAT = "AT+RST"
     setAddrAT= "AT+ADDR=2020"
@@ -26,25 +25,10 @@ def initalConfig():
     cfgAT = "AT+CFG=433000000,20,6,12,1,1,0,0,0,0,3000,8,4"
     rxAT = "AT+RX"
     saveAT = "AT+SAVE"
-    #WriteConfig
-    #sio.write(rstAT + '\r\n')
-    sio.write(cfgAT + '\r\n')
-    sio.flush()
-    sleep(2)
-    sio.write(setAddrAT + '\r\n')
-    sio.flush()
-    sleep(2)
-    sio.write(getAddrAT + '\r\n')
-    sio.flush()
-    sleep(2)
-    sio.write(saveAT + '\r\n')
-    sio.flush()
-    sleep(2)
-    sio.write(rxAT + '\r\n')
-    sio.flush()
-    sleep(2)
-    print("ich wurde ausgeführt")
-
+    commands = ([cfgAT, setAddrAT, getAddrAT, saveAT, rxAT])
+    for i in commands:
+        print(i)
+        sendCommand(i)
 
 def readSerialLine():
     while 1:
@@ -55,10 +39,6 @@ def readSerialLine():
 start_new_thread(readSerialLine,())
 initalConfig()
 while 1:
-    #i=1
-    #if i == 1:
-        #initalConfig()
-        #i=i+1
     input_val = input("> ")
     if input_val == 'exit':
         ser.close()
@@ -66,7 +46,6 @@ while 1:
     else:
         sio.write(input_val + '\r\n')
         sio.flush()
-        #print(">>"+sio.readline())
 ser.close()
 
 
