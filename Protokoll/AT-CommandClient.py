6c14491d97a2cf3b553d0c643fa07e1a0a0314e8
MichaelSchwabe <MichaelSchwabe>
2018-11-28 15:08:21 +0100

    idee für RoutingTabelle und ein paar andere ansätze

--- Protokoll/AT-CommandClient.py
+++ Protokoll/AT-CommandClient.py
@@ -3,9 +3,13 @@ import io
 import time
 from _thread import start_new_thread
 
+#serial connection
 ser = serial.Serial (port = "/dev/ttyS0")#Open named port)
 ser.timeout = 0.3
-ser.baudrate = 115200     
+ser.baudrate = 115200
+
+#RoutingTabelle {<String>,<String,Int>}
+routingTable = {"0000":{"":1}}
 
 if(not ser.isOpen()):
     ser.open()
@@ -13,14 +17,41 @@ if(not ser.isOpen()):
 sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
 print("Initializing the device ..")
 
+'''
+KDIS = Koordinator Discovery
+ADDR = Koordinator Sendet einem Client eine feste Adresse
+MSSG = einfache Nachricht
+POLL = Selbstanfrage zur Sicherstellung der eindeutigen Adresse
+DISC = Entdeckung von allen Nachbarn
+ALIV = Anfrage, ob ein Knoten noch in der Nähe ist (Eigentlich durch DISC abgedeckt)
+'''
+
+
+#NeighboorDiscovery
+def neighboorDiscovery():
+    pass
+
+
+def incrementHex(valhex):
+    return str(valhex+1)
+    #return hex(valhex+1)
+
+def checkKoordinator():
+    #koordinator hat 0000
+    
+    pass
+
+
+#CommandSend Method
 def sendCommand(command):
     sio.write(command + '\r\n')
     sio.flush()
     sio.readline()
 
+#InitialConfig methode
 def initalConfig():
     rstAT = "AT+RST"
-    setAddrAT= "AT+ADDR=2020"
+    setAddrAT= "AT+ADDR=00FF"
     getAddrAT= "AT+ADDR?"
     getDestAT= "AT+DEST?"
     cfgAT = "AT+CFG=433000000,20,9,10,1,1,0,0,0,0,3000,8,4"
@@ -31,15 +62,21 @@ def initalConfig():
         print(i)
         sendCommand(i)
 
+#ReadLine Funktion
 def readSerialLine():
     while 1:
         read = sio.readline()       
         if read != "":
             print(read)
 
+#ReadLineThread
 start_new_thread(readSerialLine,())
 
+#InitialConfig to configure the LoRa Modul
 initalConfig()
+
+
+#Main Loop überarbeiten da ich ja jetzt SendCommand besitze
 while 1:
     input_val = input("> ")
     if input_val == 'exit':
